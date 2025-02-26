0569007b31a2ae298e8510fefcdc7aa7462a6d2c
Leo Chung <leo.chung97@gmail.com>
2025-02-26 13:30:30 -0500

    Remove boilerplate code from main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-# Boiler plate starter code for Alpaca Crypto API
-from alpaca.data.historical import CryptoHistoricalDataClient
-from alpaca.data.requests import CryptoBarsRequest
-from alpaca.data.timeframe import TimeFrame
-
-client = CryptoHistoricalDataClient()
-
-request_params = CryptoBarsRequest(
-  symbol_or_symbols=["BTC/USD"],
-  timeframe=TimeFrame.Day,
-  start="2025-02-19"
-)
-
-bars = client.get_crypto_bars(request_params)
-print(bars)
+import alpaca_trade_api as tradeapi
+import time
+import pandas as pd
+from datetime import datetime, timedelta
